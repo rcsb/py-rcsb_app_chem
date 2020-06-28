d49f419ca3840c9fcd2ab21e5ab53705b5ba98c9
jwest <john.westbrook@rcsb.org>
2020-06-27 20:55:24 -0400

    Improving test coverage

--- rcsb/app/chem/ReloadDependencies.py
+++ rcsb/app/chem/ReloadDependencies.py
@@ -24,9 +24,8 @@ import platform
 import resource
 import time
 
+from rcsb.utils.chem.ChemCompDepictWrapper import ChemCompDepictWrapper
 from rcsb.utils.chem.ChemCompSearchWrapper import ChemCompSearchWrapper
-from rcsb.utils.io.MarshalUtil import MarshalUtil
-
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 TOPDIR = os.path.dirname(os.path.dirname(os.path.dirname(HERE)))
@@ -40,14 +39,7 @@ class ReloadDependencies(object):
     def __init__(self, cachePath=None, ccFileNamePrefix=None):
         self.__startTime = time.time()
         self.__cachePath = cachePath if cachePath else os.environ.get("CHEM_SEARCH_CACHE_PATH", "./CACHE")
-        ccFileNamePrefix = ccFileNamePrefix if ccFileNamePrefix else os.environ.get("CHEM_SEARCH_CC_PREFIX", "cc-abbrev")
-        configFlagFull = ccFileNamePrefix == "cc-full"
-
-        # Create the bootstrap configurations
-        self.__mU = MarshalUtil(workPath=self.__cachePath)
-        self.__makeBootstrapSearchConfig(configFlagFull)
-        self.__makeBootstrapDepictConfig(configFlagFull)
-        #
+        self.__ccFileNamePrefix = ccFileNamePrefix if ccFileNamePrefix else os.environ.get("CHEM_SEARCH_CC_PREFIX", "cc-full")
         logger.info("Starting at %s", time.strftime("%Y %m %d %H:%M:%S", time.localtime()))
 
     def shutdown(self):
@@ -57,127 +49,29 @@ class ReloadDependencies(object):
         endTime = time.time()
         logger.info("Completed at %s (%.4f seconds)", time.strftime("%Y %m %d %H:%M:%S", time.localtime()), endTime - self.__startTime)
 
-    def __makeBootstrapDepictConfig(self, configFlagFull):
-        """ Create depict configuration bootstrap file
+    def buildConfiguration(self, ccUrlTarget=None, birdUrlTarget=None):
+        """ Build bootstrap configuration files
         """
-        ok = False
+
         try:
-            if configFlagFull:
-                os.environ["CHEM_DEPICT_CACHE_PATH"] = os.path.join(self.__cachePath)
-                configDirPath = os.path.join(self.__cachePath, "config")
-                configFilePath = os.path.join(configDirPath, "depict-config.json")
-            else:
-                os.environ["CHEM_DEPICT_CACHE_PATH"] = os.path.join(self.__cachePath)
-                configDirPath = os.path.join(self.__cachePath, "config")
-                configFilePath = os.path.join(configDirPath, "depict-config.json")
-            #
-            logger.info("Updating depict configuration using %s", configFilePath)
-            #
-            imageDirPath = os.path.join(self.__cachePath, "images")
-            self.__mU.mkdir(imageDirPath)
-            configD = {"versionNumber": 0.20, "imageDirPath": imageDirPath}
-            self.__mU.mkdir(configDirPath)
-            ok = self.__mU.doExport(configFilePath, configD, fmt="json", indent=3)
-            return ok
+            configFlagFull = self.__ccFileNamePrefix == "cc-full"
+            logger.info("Update configuration for prefix %r configFlagFull %r", self.__ccFileNamePrefix, configFlagFull)
+            logger.info("Data source cc %r bird %r", ccUrlTarget, birdUrlTarget)
+            ccsw = ChemCompSearchWrapper(cachePath=self.__cachePath, ccFileNamePrefix=self.__ccFileNamePrefix)
+            ok1 = ccsw.setConfig(ccUrlTarget=ccUrlTarget, birdUrlTarget=birdUrlTarget)
+            ccdw = ChemCompDepictWrapper()
+            ok2 = ccdw.setConfig(self.__cachePath)
+            return ok1 and ok2
         except Exception as e:
             logger.exception("Failing with %s", str(e))
-        return ok
-        #
-
-    def __makeBootstrapSearchConfig(self, configFlagFull):
-        """ Create search configuration bootstrap file
-        """
-        ok = False
-        try:
-            if configFlagFull:
-
-                os.environ["CHEM_SEARCH_CACHE_PATH"] = os.path.join(self.__cachePath)
-                os.environ["CHEM_SEARCH_CC_PREFIX"] = "cc-full"
-                ccFileNamePrefix = "cc-full"
-                configDirPath = os.path.join(self.__cachePath, "config")
-                configFilePath = os.path.join(configDirPath, ccFileNamePrefix + "-config.json")
-                oeFileNamePrefix = "oe-full"
-                ccFileNamePrefix = "cc-full"
-                ccUrlTarget = None
-                birdUrlTarget = None
-
-            else:
-                dataPath = os.path.join(TOPDIR, "rcsb", "app", "tests-chem", "test-data")
-                os.environ["CHEM_SEARCH_CACHE_PATH"] = os.path.join(self.__cachePath)
-                os.environ["CHEM_SEARCH_CC_PREFIX"] = "cc-abbrev"
-                ccUrlTarget = os.path.join(dataPath, "components-abbrev.cif")
-                birdUrlTarget = os.path.join(dataPath, "prdcc-abbrev.cif")
-                ccFileNamePrefix = "cc-abbrev"
-                configDirPath = os.path.join(self.__cachePath, "config")
-                configFilePath = os.path.join(configDirPath, ccFileNamePrefix + "-config.json")
-                oeFileNamePrefix = "oe-abbrev"
-            #
-            logger.info("Updating search configuration using %s and %s", ccFileNamePrefix, self.__cachePath)
-            molLimit = None
-            useCache = False
-            logSizes = False
-            #
-            numProc = 12
-            maxProc = os.cpu_count()
-            numProc = min(numProc, maxProc)
-            maxChunkSize = 10
-            logger.info("+++ >>> Using MAXPROC %d", numProc)
-            #
-            limitPerceptions = False
-            quietFlag = True
-            #
-            # fpTypeCuttoffD = {"TREE": 0.6, "MACCS": 0.9, "PATH": 0.6, "CIRCULAR": 0.6, "LINGO": 0.9}
-            fpTypeCuttoffD = {"TREE": 0.6, "MACCS": 0.9}
-            buildTypeList = ["oe-iso-smiles", "oe-smiles", "cactvs-iso-smiles", "cactvs-smiles", "inchi"]
-            #
-            oesmpKwargs = {
-                "ccUrlTarget": ccUrlTarget,
-                "birdUrlTarget": birdUrlTarget,
-                "cachePath": self.__cachePath,
-                "useCache": useCache,
-                "ccFileNamePrefix": ccFileNamePrefix,
-                "oeFileNamePrefix": oeFileNamePrefix,
-                "limitPerceptions": limitPerceptions,
-                "minCount": None,
-                "maxFpResults": 50,
-                "fpTypeCuttoffD": fpTypeCuttoffD,
-                "buildTypeList": buildTypeList,
-                "screenTypeList": None,
-                "quietFlag": quietFlag,
-                "numProc": numProc,
-                "maxChunkSize": maxChunkSize,
-                "molLimit": molLimit,
-                "logSizes": logSizes,
-            }
-            ccsiKwargs = {
-                "ccUrlTarget": ccUrlTarget,
-                "birdUrlTarget": birdUrlTarget,
-                "cachePath": self.__cachePath,
-                "useCache": useCache,
-                "ccFileNamePrefix": ccFileNamePrefix,
-                "oeFileNamePrefix": oeFileNamePrefix,
-                "limitPerceptions": limitPerceptions,
-                "minCount": None,
-                "numProc": numProc,
-                "quietFlag": quietFlag,
-                "maxChunkSize": maxChunkSize,
-                "molLimit": None,
-                "logSizes": False,
-            }
-            configD = {"versionNumber": 0.20, "ccsiKwargs": ccsiKwargs, "oesmpKwargs": oesmpKwargs}
-            self.__mU.mkdir(configDirPath)
-            ok = self.__mU.doExport(configFilePath, configD, fmt="json", indent=3)
-            return ok
-        except Exception as e:
-            logger.exception("Failing with %s", str(e))
-        return ok
+        return False
 
     def updateDependencies(self):
-        """Test update search index.
+        """ Rebuild search indices using configuration files.
         """
         try:
-            logger.info("Starting update %r in %r", os.environ["CHEM_SEARCH_CC_PREFIX"], os.environ["CHEM_SEARCH_CACHE_PATH"])
-            ccsw = ChemCompSearchWrapper()
+            logger.info("Starting update %r in %r", self.__ccFileNamePrefix, self.__cachePath)
+            ccsw = ChemCompSearchWrapper(cachePath=self.__cachePath, ccFileNamePrefix=self.__ccFileNamePrefix)
             ok1 = ccsw.readConfig()
             ok2 = ccsw.updateChemCompIndex()
             ok3 = ccsw.updateSearchIndex()
--- rcsb/app/tests-chem/fixtureChemCompSearchWrapper.py
+++ rcsb/app/tests-chem/fixtureChemCompSearchWrapper.py
@@ -26,11 +26,7 @@ import time
 import unittest
 
 from rcsb.app.chem import __version__
-
-# from rcsb.app.chem.ReloadDependencies import ReloadDependencies
-
-from rcsb.utils.chem.ChemCompDepictWrapper import ChemCompDepictWrapper
-from rcsb.utils.chem.ChemCompSearchWrapper import ChemCompSearchWrapper
+from rcsb.app.chem.ReloadDependencies import ReloadDependencies
 
 
 HERE = os.path.abspath(os.path.dirname(__file__))
@@ -44,14 +40,21 @@ logger.setLevel(logging.INFO)
 class ChemCompSearchWrapperFixture(unittest.TestCase):
     def setUp(self):
         self.__startTime = time.time()
-        self.__testFlagFull = os.environ.get("CHEM_SEARCH_CC_PREFIX", "cc-abbrev") == "cc-full"
-        self.__workPath = os.path.join(HERE, "test-output")
-        testFlagFull = False
-        dataPath = os.path.join(HERE, "test-data")
+        configFlagFull = False
         self.__cachePath = os.path.join(HERE, "test-output", "CACHE")
-        self.__ccFileNamePrefix = "cc-abbrev"
-        self.__testBuildConfiguration(self.__cachePath, dataPath, self.__ccFileNamePrefix, testFlagFull)
+        dataPath = os.path.join(HERE, "test-data")
+        if not configFlagFull:
+            self.__ccFileNamePrefix = "cc-abbrev"
+            ccUrlTarget = os.path.join(dataPath, "components-abbrev.cif") if not configFlagFull else None
+            birdUrlTarget = os.path.join(dataPath, "prdcc-abbrev.cif") if not configFlagFull else None
+        else:
+            self.__ccFileNamePrefix = "cc-full"
+            ccUrlTarget = birdUrlTarget = None
 
+        rD = ReloadDependencies(self.__cachePath, self.__ccFileNamePrefix)
+        rD.buildConfiguration(ccUrlTarget=ccUrlTarget, birdUrlTarget=birdUrlTarget)
+        rD.shutdown()
+        #
         logger.debug("Running tests on version %s", __version__)
         logger.info("Starting %s at %s", self.id(), time.strftime("%Y %m %d %H:%M:%S", time.localtime()))
 
@@ -62,42 +65,14 @@ class ChemCompSearchWrapperFixture(unittest.TestCase):
         endTime = time.time()
         logger.info("Completed %s at %s (%.4f seconds)", self.id(), time.strftime("%Y %m %d %H:%M:%S", time.localtime()), endTime - self.__startTime)
 
-    def __testBuildConfiguration(self, cachePath, dataPath, ccFileNamePrefix, testFlagFull):
-        """Build bootstrap configuration
-        """
-        try:
-            logger.info("Update configuration for prefix %r testFlagFull %r", ccFileNamePrefix, testFlagFull)
-            ccsw = ChemCompSearchWrapper(cachePath=cachePath, ccFileNamePrefix=ccFileNamePrefix)
-            ccUrlTarget = os.path.join(dataPath, "components-abbrev.cif") if not testFlagFull else None
-            birdUrlTarget = os.path.join(dataPath, "prdcc-abbrev.cif") if not testFlagFull else None
-            logger.info("Data source cc %r bird %r", ccUrlTarget, birdUrlTarget)
-            ok = ccsw.setConfig(ccUrlTarget=ccUrlTarget, birdUrlTarget=birdUrlTarget)
-            self.assertTrue(ok)
-            #
-            ccdw = ChemCompDepictWrapper()
-            ok = ccdw.setConfig(cachePath)
-            self.assertTrue(ok)
-        except Exception as e:
-            logger.exception("Failing with %s", str(e))
-            self.fail()
-
     def testUpdateDependencies(self):
-        """Test update search index.
+        """Test update search indices.
         """
         try:
-            logger.info("Updating dependences for prefix %r", self.__ccFileNamePrefix)
-            ccsw = ChemCompSearchWrapper(cachePath=self.__cachePath, ccFileNamePrefix=self.__ccFileNamePrefix)
-            ok = ccsw.readConfig()
+            rD = ReloadDependencies(self.__cachePath, self.__ccFileNamePrefix)
+            ok = rD.updateDependencies()
             self.assertTrue(ok)
-            ok = ccsw.updateChemCompIndex()
-            self.assertTrue(ok)
-            ok = ccsw.updateSearchIndex()
-            self.assertTrue(ok)
-            ok = ccsw.updateSearchMoleculeProvider()
-            self.assertTrue(ok)
-            # verify access -
-            ok = ccsw.reloadSearchDatabase()
-            self.assertTrue(ok)
+            rD.shutdown()
         except Exception as e:
             logger.exception("Failing with %s", str(e))
             self.fail()

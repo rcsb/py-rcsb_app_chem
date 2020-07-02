78290aa66a69605ac05e50a1b565f87434da2270
jwest <john.westbrook@rcsb.org>
2020-07-01 20:38:19 -0400

    V0.23 Further update of dependencies

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     ),
     entry_points={"console_scripts": []},
     #
-    install_requires=["fastapi >= 0.55.1", "aiofiles >= 0.5.0", "uvicorn >= 0.11.5", "pydantic >= 1.4", "rcsb.utils.io >= 0.69", "rcsb.utils.chem >= 0.38", "gunicorn >= 20.0.4"],
+    install_requires=["fastapi >= 0.55.1", "aiofiles >= 0.5.0", "uvicorn >= 0.11.5", "pydantic >= 1.4", "rcsb.utils.io >= 0.69", "rcsb.utils.chem >= 0.39", "gunicorn >= 20.0.4"],
     packages=find_packages(exclude=["rcsb.app.tests-*", "tests.*"]),
     package_data={
         # If any package contains *.md or *.rst ...  files, include them:

bb4b95465eead7f4098e0f8b1bf20dcd940867e1
Jeremy Henry <jeremy.henry@rcsb.org>
2023-07-26 16:06:36 -0700

    Add a stub for mounting the license and clean up the launch script

--- scripts/LAUNCH_GUNICORN.sh
+++ scripts/LAUNCH_GUNICORN.sh
@@ -7,24 +7,18 @@
 #     nohup ./scripts/LAUNCH_GUNICORN.sh >& LOGTODAY
 ##
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-TOPDIR="$(dirname "$HERE")"
-OE_LICENSE=${OE_LICENSE:=~/oe_license.txt}
 echo "HERE=${HERE}"
-echo "TOPDIR=${TOPDIR}"
-echo "OE_LICENSE=$OE_LICENSE"
 
 THISIP=${HOSTIP:="0.0.0.0"}
 THISPORT=${HOSTPORT:="8000"}
 ADDR=${THISIP}:${THISPORT}
 
-#unset CHEM_SEARCH_DATA_HOSTNAME
-#unset CHEM_SEARCH_DATA_PATH
 export CHEM_SEARCH_CC_PREFIX="cc-full"
 export CHEM_SEARCH_CACHE_PATH=${HERE}/CACHE
 export CHEM_DEPICT_CACHE_PATH=${HERE}/CACHE
 export GIT_PYTHON_REFRESH=quiet
 
-/home/ubuntu/.local/bin/gunicorn \
+gunicorn \
 rcsb.app.chem.main:app \
     --timeout 300 \
     --chdir ${HERE} \

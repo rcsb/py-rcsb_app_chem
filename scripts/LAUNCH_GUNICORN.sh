dd3dfb4014abf44c22eb36eedbf647effa5f6b3f
Aditya Pingale <aditya.pingale@rcsb.org>
2024-08-07 14:30:56 -0400

    accommodate openstack deployment

--- scripts/LAUNCH_GUNICORN.sh
+++ scripts/LAUNCH_GUNICORN.sh
@@ -9,14 +9,20 @@
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "HERE=${HERE}"
 
-THISIP=${HOSTIP:="0.0.0.0"}
-THISPORT=${HOSTPORT:="8000"}
-ADDR=${THISIP}:${THISPORT}
-
 export CHEM_SEARCH_CC_PREFIX="cc-full"
 export CHEM_SEARCH_CACHE_PATH=${HERE}/CACHE
 export CHEM_DEPICT_CACHE_PATH=${HERE}/CACHE
 export GIT_PYTHON_REFRESH=quiet
+
+if grep -q docker /proc/1/cgroup; then 
+    echo "inside docker container"
+    THISIP=${HOSTIP:="0.0.0.0"}
+else
+    echo "on openstack host"
+    THISIP=${HOSTIP:="127.0.0.1"}
+fi
+THISPORT=${HOSTPORT:="8000"}
+ADDR=${THISIP}:${THISPORT}
 
 gunicorn \
 rcsb.app.chem.main:app \
